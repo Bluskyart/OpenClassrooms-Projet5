94f93ab44367c5c0bfd83bb8620be2bc31af064c
Guerwan Lescrohart <glescrohart@gmail.com>
2024-10-18 03:25:36 +0200

    feat: CSS page logement + Slideshow

--- src/pages/Logement.jsx
+++ src/pages/Logement.jsx
@@ -1,38 +1,49 @@
 import React from "react";
-import { useParams } from "react-router-dom";
+import { useParams, Navigate } from "react-router-dom";
 import Collapse from "../components/Collapse.jsx"
 import Data from "../Datas/logements.json";
 import '../styles/collapse.scss';
+import '../styles/logement.scss';
+import Slideshow from '../components/Slideshow.jsx';
 
 
 function Logement() {
     const { id } = useParams();
-
+    const ficheLogement = Data.find((logement) => logement.id === id);
     const logement = Data.find((item) => item.id === id);
 
     return (
-        <div className="logement">
-            <main>
-                <div className="collapsibles ">
-                    <div className="collapse__dropdown">
-                        <div key={logement.id}>
-                            <h2>{logement.title}</h2>
-                            <div>
-                                <img src={logement.host.picture} alt="portrait"/>
-                                <h2>{logement.host.name}</h2>
+        <>
+        {ficheLogement ? (
+            <div className="logement">
+                <main>
+                    <div>
+                        <Slideshow slides={logement.pictures} />
+                        <div className="collapse__dropdown">
+                            <div key={logement.id}>
+                                <div className="logement_infos">
+                                    <h2 className="logement_title">{logement.title}</h2>
+                                    <h2 className="logement_location">{logement.location}</h2>
+                                </div>
+                                <div className="host_identity">
+                                    <img className="host_picture" src={logement.host.picture} alt="portrait"/>
+                                    <h2 className="host_name">{logement.host.name}</h2>
+                                </div>
+                                
+                                <h2 className="logement_all_tags">{logement.tags.map((element, index) => {
+                                return(<p className='logement_tags' key={"tags-"+index}>{element}</p>)
+                            })}</h2>
+                                <div className="collapsibles">
+                                    <Collapse content={logement.description} title="Description" />
+                                    <Collapse content={logement.equipments} title="Equipements" />
+                                </div>
                             </div>
-                            <h2>{logement.location}</h2>
-                            <h2>{logement.tags}</h2>
-                            <div>
-                                <Collapse content={logement.description} title="Description" />
-                                <Collapse content={logement.equipments} title="Equipements" />
-                            </div>
-                            
                         </div>
                     </div>
-                </div>
-            </main>
-        </div>
+                </main>
+            </div>
+        ) : (<Navigate replace to="/404" />)}
+        </>
     );
 };
 

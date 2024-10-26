eccac60e9919038f3105ee10763662883094adbe
Guerwan Lescrohart <glescrohart@gmail.com>
2024-10-26 12:14:53 +0200

    fix: collapse's arrows and slider animation + media queries

--- src/components/Collapse.jsx
+++ src/components/Collapse.jsx
@@ -1,25 +1,27 @@
-import {React, useState} from 'react';
+import {React, useRef, useState} from 'react';
 import '../styles/collapse.scss';
-import chevronDown from '../assets/chevron-down.svg';
+import chevronUp from '../assets/chevron-up.svg';
 
 function Collapse({title, content}) {
-    const [contentVisible, setContentVisible] = useState(false)
+    const [contentVisible, setContentVisible] = useState(false);
+    const contentRef = useRef(null);
 
     const affContent = () => { 
         setContentVisible(!contentVisible);
     };
 
     const chevronRotation = contentVisible ? "rotate" : "";
+    const contentHeight = contentVisible ? contentRef.current.scrollHeight : 0;
 
     return (
         <div className='collapse'>
             <div className='collapse__header' onClick={affContent}>
                 <span>{title}</span>
                 <div className="chevronValue">
-                    <img className={chevronRotation} src={chevronDown} alt="chevron"/>
+                    <img className={chevronRotation} src={chevronUp} alt="chevron"/>
                 </div>
             </div>
-            <div className={contentVisible ? "visible_collapse" : "hidden collapse"}>
+            <div ref={contentRef} className="collapse__content" style={{ maxHeight: contentHeight, transition: 'max-height 0.5s ease' }}>
                 {
                     typeof content === 'string' ? (
                         <p>{content}</p>
